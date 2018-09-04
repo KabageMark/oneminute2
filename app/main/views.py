ddb6d5b809bbf7fa4ea7a53ee25f79db0ba10558
kabagemark <kabagemark@gmail.com>
2018-09-04 10:31:25 +0300

    register route added

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,21 @@
 from flask_login import login_required
+
+
+
+
+
+
+
+
+
+
+@main.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
