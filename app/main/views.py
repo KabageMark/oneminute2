766e281b2a5c88c5cc34bbd052e1d89559466ae3
kabagemark <kabagemark@gmail.com>
2018-09-04 10:43:14 +0300

    login and registration imports made in views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
 from flask_login import login_required
-
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user
+from ..models import User
+from .forms import LoginForm,RegistrationForm
 
 
 
