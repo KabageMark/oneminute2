e6d43e661a5268db36f4b5a3c73315a05cd8c357
kabagemark <kabagemark@gmail.com>
2018-09-04 10:40:02 +0300

    loginForm class moved to forms from models

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,12 @@ from ..models import User
 from wtforms import ValidationError
 
 
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
 
 
 
