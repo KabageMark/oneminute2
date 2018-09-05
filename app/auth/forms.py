a4cb53fbfe2770612e0886f7b5dfc32c2f091fab
kabagemark <kabagemark@gmail.com>
2018-09-05 12:39:27 +0300

    database uri added to class config

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+from .. models import User
 from wtforms import ValidationError
 
 
@@ -10,12 +10,6 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
-
-
-
-
-
-
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://markchege:newpassword@localhost/pitchusers'
+
     
     
 class ProdConfig(Config):
