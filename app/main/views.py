5cdcbfe991f347d5a91cc96f076785799d1c69cf
kabagemark <kabagemark@gmail.com>
2018-09-07 16:45:51 +0300

    registration form added

--- app/main/views.py
+++ app/main/views.py
@@ -47,7 +47,7 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data,confirm_password = form.password_confirm.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('.login'))
